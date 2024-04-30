6415509a548f6e79dbc0be10a13021f0f9b55c21
HienXuanPham <phamxuanhien02@gmail.com>
2024-04-30 12:51:52 -0400

    setup PostgreSQL database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    if not test_config:
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
 
